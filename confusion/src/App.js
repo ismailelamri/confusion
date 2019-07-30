e0d8256378b4459e35db23a1359aac60fcb0c6c7
Ismail El Amri <ielamri@isimtl.local>
2019-07-30 19:51:15 -0400

    component part one

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand } from 'reactstrap'
+import Menu from './component/MenuComponent';
 import './App.css';
 
 class App extends Component {
 render(){
 return (
-    <div className="App">
-      <Navbar dark color ="primary">
+    <div >
+      <Navbar dark  color ="primary">
         <div className="container">
-          <NavbarBrand href ="/">Ristorante con fusion </NavbarBrand>
+          <NavbarBrand href ="/">Menu</NavbarBrand>
+          <NavbarBrand href ="/">Service</NavbarBrand>
+          <NavbarBrand href ="/">About</NavbarBrand>
+          <NavbarBrand href ="/">Our story</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
